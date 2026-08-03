da220bd87fc5c3d438b75eb4d783aedeaecffcac
Laya-2024 <layasree70@gmail.com>
2026-08-03 20:50:56 +0530

    Change password for emergency_app user
    Updated the password for the 'emergency_app' user.

--- mysql_user_setup.sql
+++ mysql_user_setup.sql
@@ -6,8 +6,8 @@
 CREATE DATABASE IF NOT EXISTS emergency_response CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER IF NOT EXISTS 'emergency_app'@'localhost'
-  IDENTIFIED BY 'Laya@2024';
+  IDENTIFIED BY 'XXXXXX;
 ALTER USER 'emergency_app'@'localhost'
-  IDENTIFIED BY 'Laya@2024';
+  IDENTIFIED BY 'XXXXXX';
 GRANT ALL PRIVILEGES ON emergency_response.* TO 'emergency_app'@'localhost';
 FLUSH PRIVILEGES;
